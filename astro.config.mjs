51d0e28e8699cd1451e9e517aad56dbafb6bfa47
Carlos Sánchez Recio <carlos.sanchez@perlatours.com>
2025-07-09 09:28:03 +0200

    feat: add tsconfig alias and vite config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,17 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import path from "node:path";
+import { fileURLToPath } from "node:url";
 
-// https://astro.build/config
-export default defineConfig({});
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default defineConfig({
+	vite: {
+		resolve: {
+			alias: {
+				"@": path.resolve(__dirname, "src"),
+				"@styles": path.resolve(__dirname, "src/styles")
+			}
+		}
+	}
+});
